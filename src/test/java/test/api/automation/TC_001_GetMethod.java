0b7e1a50bcccd37d7ee5eec8dabd1014e9f7705a
Iqbal <iqbalchoudhury2@gmail.com>
2019-03-15 08:40:42 -0400

    Added BDD Method

--- src/test/java/test/api/automation/TC_001_GetMethod.java
+++ src/test/java/test/api/automation/TC_001_GetMethod.java
@@ -2,26 +2,44 @@ package test.api.automation;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import static io.restassured.RestAssured.*;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class TC_001_GetMethod {
-	
+
 	@Test
 	public void tc_001_getMethod_validation() {
 		Response response = RestAssured.get("http://services.groupkt.com/state/search/IND?text=pradesh");
 		System.out.println(response.asString());
-		System.out.println("Status response code \t \t"+ response.getStatusCode());
-		Assert.assertEquals(200,response.getStatusCode());
-		
-		
-		
+		System.out.println("Status response code \t \t" + response.getStatusCode());
+		Assert.assertEquals(200, response.getStatusCode());
+
 	}
+
 	@Test
 	public void tc_001_getMethod_contentType() {
-		Response res=RestAssured.get("http://services.groupkt.com/state/get/IND/UP");
-		Assert.assertEquals(res.getContentType(),"application/json;charset=UTF-8");
-		
+		Response res = RestAssured.get("http://services.groupkt.com/state/get/IND/UP");
+		Assert.assertEquals(res.getContentType(), "application/json;charset=UTF-8");
+
 	}
+
+	@Test
+	public void tc_001_getMethod_validationStatic() {
+		Response response = get("http://services.groupkt.com/state/search/IND?text=pradesh");
+		System.out.println(response.asString());
+		System.out.println("Status response code \t \t" + response.getStatusCode());
+		Assert.assertEquals(200, response.getStatusCode());
+	}
+
+	@Test
+	public void tc_001_getMethod_validationStaticWithBDD() {
+		Response response = get("http://services.groupkt.com/state/search/IND?text=pradesh");
+		System.out.println(response.asString());
+		System.out.println("Status response code \t \t" + response.getStatusCode());
+		Assert.assertEquals(200, response.getStatusCode());
+
+		when().get("http://services.groupkt.com/state/search/IND?text=pradesh").then().statusCode(200);
+	}
+
 }
